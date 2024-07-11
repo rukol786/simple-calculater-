79fe703270f5fe5d20c488d16d59eae930ec1418
Sana <rukol786786@gmail.com>
2024-07-11 12:07:23 -0700

    Add files via upload

---
+++ math.js
@@ -0,0 +1,29 @@
+function multiplyBy()
+{
+
+    num1=document.getElementById("firstNumber").value;
+    num2=document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML=num1*num2;
+
+}
+
+function divideBy()
+{
+     num1=document.getElementById("firstNumber").value;
+     num2=document.getElementById("secondNumber").value;
+     document.getElementById("result").innerHTML=num1/num2;
+
+     function PercentageBy()
+{
+     num1=document.getElementById("firstNumber").value;
+     num2=document.getElementById("secondNumber").value;
+     document.getElementById("result").innerHTML=num2/num1*100;
+}
+
+}
+function subtractionBy()
+{
+     num1=document.getElementById("firstNumber").value;
+     num2=document.getElementById("secondNumber").value;
+     document.getElementById("result").innerHTML=num1-num2;
+}
